20940e0132c4e1551b390d52016da0c1215a6228
Don <don.r.kabob.trading@gmail.com>
2024-05-14 16:45:44 -0700

    Changes to titles and headers

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -168,7 +168,7 @@ def __account_change(client=None):
 
 def position_filtering(con: st.container):
     with con:
-        st.title("Position Review")
+        st.header("Position Review")
         filter_field = st.selectbox(
             "Filter",
             ["%OTM"]
